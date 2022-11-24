e497d9ce801554e73607bd08619135ae2deae31c
Fabian Mäurer <fabianmaeurer@gmail.com>
2022-11-24 16:32:15 +0100

    sync

--- test/S-MatrixTest.jl
+++ test/S-MatrixTest.jl
@@ -1,15 +1,12 @@
-#=------------------------------------------------
-    Test S-Matrix of Center of I₂(5) by comparison
-    with Lusztigs and Malles
-------------------------------------------------=#
+using TensorCategories, Oscar
 
 # To speed up computation compute the equivalent centre category category
 I = I2subcategory(5)
 C = Center(I)
 simples(C)
 
-#Sort the simples in 𝒵(I₂(5)) to match Lusztigs
-C.simples = C.simples[[]]
+#Sort the simples in 𝒵(I₂(5)) to match Lusztigs and Malles
+C.simples = C.simples[[2,1,3,4]]
 
 S = normalized_smatrix(C)
 
@@ -18,20 +15,21 @@ S = normalized_smatrix(C)
 
 K = base_ring(I)
 ξ = gen(K)
-λ = -ξ^3 + ξ^2 + 1
+λ = -ξ^3 + ξ^2 + 1 #(1 + √5)/2
 
 Lusztigs_S = inv(sqrt(K(5))) * matrix(K,[  λ-1 λ   1    1;
                         λ   λ-1 -1   -1;
                         1   -1  λ    -λ+1;
                         1   -1  -λ+1 λ])
 
-# Malle S-matrix according to 
+# S-matrix according to Geck, Malle
 # https://www.sciencedirect.com/science/article/pii/S0021869302006312
 
 tuples = [(i,j) for i ∈ 0:5, j ∈ 0:5 if 0 < i < j < i+j < 5 || 0 == i < j < 5/2]
 
-Malles_S = matrix(K, [inv(K(5)) * (ξ^(i*l+j*k) + ξ^(-i*l-j*k) - ξ^(i*k + j*l) - ξ^(-i*k-j*l)) for (i,j) ∈ tuples, (k,l) ∈ tuples])
+Lusztigs_S_formel = matrix(K, [inv(K(5)) * ((ξ^2)^(-i*l+j*k) + (ξ^2)^(i*l-j*k) - (ξ^2)^(-i*k + j*l) - (ξ^2)^(i*k-j*l)) for (i,j) ∈ tuples, (k,l) ∈ tuples])
 
-@testset "𝒵(I₂(5)) S-matrix" begin
-    @test "S == "
-end
+Geck_Malle_S_10 = matrix(K, [inv(K(5)) * (ξ^(i*l+j*k) + ξ^(-i*l-j*k) - ξ^(i*k + j*l) - ξ^(-i*k-j*l)) for (i,j) ∈ tuples, (k,l) ∈ tuples])
+
+Geck_Malle_S_5 = matrix(K, [inv(K(5)) * ((ξ^2)^(i*l+j*k) + (ξ^2)^(-i*l-j*k) - (ξ^2)^(i*k + j*l) - (ξ^2)^(-i*k-j*l)) for (i,j) ∈ tuples, (k,l) ∈ tuples])
+
